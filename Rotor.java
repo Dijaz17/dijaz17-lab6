209b88c5a82edf1a5b8fdb8ff7e3625e5c7eaf09
Dijaz17 <87551242+Dijaz17@users.noreply.github.com>
2025-02-23 18:33:11 -0500

    rotor methods

--- Rotor.java
+++ Rotor.java
@@ -3,6 +3,8 @@ public class Rotor {
     
     private String rotorValues;
     private char startChar;
+    private 
+    char[] rValsArr = rotorValues.toCharArray();
         
     public Rotor(String v, char c){
         this.rotorValues = new String(v);
@@ -14,16 +16,27 @@ public class Rotor {
     
     public boolean rotate(){
         //TODO
-               
+        if(this.startChar == rValsArr[0]){
+            return true;
+        }
+        return false;
     }
     
 
     public int indexOf(char c){
         //TODO
+        int idx = -1;
+        for(int i = 0; i < rValsArr.length; i++){
+            if( c == rValsArr[i] ){
+                idx = i;
+            }
+        }
+        return idx;
     }
 
     public char charAt(int idx){
         //TODO
+        return rValsArr[idx];
     }
 }
     
