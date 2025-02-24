d7c052d06e1af9122168e3d9ac6f321864a9a50f
Dijaz17 <87551242+Dijaz17@users.noreply.github.com>
2025-02-23 19:21:32 -0500

    rotor class complete

--- Rotor.java
+++ Rotor.java
@@ -3,19 +3,20 @@ public class Rotor {
     
     private String rotorValues;
     private char startChar;
-    private 
-    char[] rValsArr = rotorValues.toCharArray();
         
     public Rotor(String v, char c){
         this.rotorValues = new String(v);
         this.startChar = c;
         
-        while(!this.rotate());
+        while(!this.rotate()){
+            rotateRotor();
+        }
             
     }
     
     public boolean rotate(){
         //TODO
+        char[] rValsArr = rotorValues.toCharArray();
         if(this.startChar == rValsArr[0]){
             return true;
         }
@@ -26,6 +27,8 @@ public class Rotor {
     public int indexOf(char c){
         //TODO
         int idx = -1;
+        char[] rValsArr = rotorValues.toCharArray();
+
         for(int i = 0; i < rValsArr.length; i++){
             if( c == rValsArr[i] ){
                 idx = i;
@@ -36,7 +39,19 @@ public class Rotor {
 
     public char charAt(int idx){
         //TODO
+        char[] rValsArr = rotorValues.toCharArray();
         return rValsArr[idx];
+    }
+
+    public void rotateRotor(){
+        char[] rValsArr = rotorValues.toCharArray();
+        String rotatedString = "";
+        rotatedString += rValsArr[26];
+        for(int i = 1; i < rValsArr.length-1; i++){
+            rotatedString += rValsArr[i];
+        }
+
+        rotorValues = rotatedString;
     }
 }
     
